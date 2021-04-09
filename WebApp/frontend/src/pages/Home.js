5dca0bb4fd840089345214d8af521c84889104dc
Vijaylaxmi Nagurkar <vijaylaxmidighe@gmail.com>
2021-04-08 17:12:09 -0700

    Okta integration

--- WebApp/frontend/src/pages/Home.js
+++ WebApp/frontend/src/pages/Home.js
@@ -4,5 +4,5 @@ import {
 } from '@material-ui/core';
 
 export default () => (
-  <Typography variant="h4">Welcome Home!</Typography>
+  <Typography variant="h4">Welcome to stock trading application!</Typography>
 );
