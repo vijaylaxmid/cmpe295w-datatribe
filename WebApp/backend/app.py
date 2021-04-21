ec3f7d20a0150ae45ff30870a3f77201b6861c35
Vijaylaxmi Nagurkar <vijaylaxmidighe@gmail.com>
2021-04-20 17:44:59 -0700

    removed unused code

--- WebApp/backend/app.py
+++ WebApp/backend/app.py
@@ -80,46 +80,7 @@ def add_transction():
 
   return transaction_schema.jsonify(new_transaction)
 
-  # Get All Products
-@app.route('/product', methods=['GET'])
-def get_products():
-  all_products = Product.query.all()
-  result = products_schema.dump(all_products)
-  return jsonify(result)
-
-  # Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
-
-  # Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
-
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
-
-  product.name = name
-  product.description = description
-  product.price = price
-  product.qty = qty
-
-  db.session.commit()
-
-  return product_schema.jsonify(product)
-
-  # Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  db.session.delete(product)
-  db.session.commit()
-
-  return product_schema.jsonify(product)
+  
 
 
 #Run server
