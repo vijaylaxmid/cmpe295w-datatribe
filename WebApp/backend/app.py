b8d45811933c27f61f138846697e2c592ca8b461
Vijaylaxmi Nagurkar <vijaylaxmidighe@gmail.com>
2021-04-20 17:46:06 -0700

    removed unused code

--- WebApp/backend/app.py
+++ WebApp/backend/app.py
@@ -19,19 +19,6 @@ db = SQLAlchemy(app)
 #init ma
 ma = Marshmallow(app)
 
-# Product Class/Model
-#class Product(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   name = db.Column(db.String(100), unique=True)
-#   description = db.Column(db.String(200))
-#   price = db.Column(db.Float)
-#   qty = db.Column(db.Integer)
-
-#   def __init__(self, name, description, price, qty):
-#     self.name = name
-#     self.description = description
-#     self.price = price
-#     self.qty = qty
 
  #Transaction Class/Model
 class Transaction(db.Model):
